bad02b97c7772045ff0082ebe83c887d5da9baa0
Tawanda Tafara <tbwerinofa@yahoo.com.au>
2023-07-27 12:01:38 +0200

    Refresh after clicking back

--- SWCTracker/wwwroot/js/client.js
+++ SWCTracker/wwwroot/js/client.js
@@ -1,31 +1,37 @@
 ﻿$occupationGroupId = $('#OccupationGroupId');
 $occupationId = $('#OccupationId');
 $getOccupationByParentId = '/Calculator/GetOccupationSelectListItem_ByParentId';
+
+
 $(document).ready(function () {
 
     if ($('#form-home').length > 0) {
-        OccupationGroupChange();
-    }
+        $occupationGroupId = $('#OccupationGroupId');
+        $occupationId = $('#OccupationId');
+            OccupationGroupChange();
+        }
+   
 
     $occupationGroupId.change(function (evt) {
         OccupationGroupChange();
     })
 
-    function OccupationGroupChange() {
-
-        DropDowns.LoadData(emptyObject, $occupationId, 0);
-
-        if ($occupationGroupId.val() !== "0") {
-
-            AjaxPostBack.Get($getOccupationByParentId, {
-                'parentId': $occupationGroupId.val()
-            }, PopulateChild);
-        }
-    }
-
-    function PopulateChild(response) {
-        DropDowns.LoadData(response.Response, $occupationId, 0, true)
-    }
-
 
 });
+
+
+function OccupationGroupChange() {
+
+    DropDowns.LoadData(emptyObject, $occupationId, 0);
+
+    if ($occupationGroupId.val() !== "0") {
+
+        AjaxPostBack.Get($getOccupationByParentId, {
+            'parentId': $occupationGroupId.val()
+        }, PopulateChild);
+    }
+}
+
+function PopulateChild(response) {
+    DropDowns.LoadData(response.Response, $occupationId, 0, true)
+}
