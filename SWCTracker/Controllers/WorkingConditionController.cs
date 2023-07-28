12f3240f31dc368ffd2e565ef4b2ff812d75c403
Tawanda Tafara <tbwerinofa@yahoo.com.au>
2023-07-28 12:44:31 +0200

    Implement Grade Page

---
+++ SWCTracker/Controllers/WorkingConditionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SWCTracker.Controllers
+{
+    public class WorkingConditionController : Controller
+    {
+        // GET: WorkingConditionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
